2239a52843c05280ccae7c621c1a569ea96123df
Stan <guseletovS@gmail.com>
2021-08-11 16:55:31 +0100

    started

--- add-element.js
+++ add-element.js
@@ -8,5 +8,18 @@ const newContent = document.createTextNode("Hi there and greetings!");
 newDiv.appendChild(newContent);
 
 // add the newly created element and its content into the DOM
-const existingDiv = document.getElementById("my-target-element");
+let existingDiv = document.getElementById("my-target-element");
 document.body.appendChild(newDiv, existingDiv);
+
+// create a new div element
+const newEl = document.createElement("Element");
+
+// and give it some content
+const newElCont = document.createTextNode("I guess this does things");
+
+// add the text node to the newly created div
+newEl.appendChild(newElCont);
+
+// add the newly created element and its content into the DOM
+existingDiv = document.getElementById("my-target-element");
+document.body.appendChild(newEl, existingDiv);
